eea281f2b6c1ac79dd763c55b447286a711feeb5
Faraz Shahid <fraz_shahid@hotmail.com>
2025-01-05 13:19:49 +0500

    Update in the cut type is added

--- DatabaseScript/UpdateScripts.sql
+++ DatabaseScript/UpdateScripts.sql
@@ -5,3 +5,9 @@ ADD COLUMN `OrderPriority` INT NULL AFTER `UpdatedBy`;
 
 ALTER TABLE `zof_mrp`.`orderitems` 
 ADD COLUMN `OrderItemPriority` INT NULL AFTER `UpdatedBy`;
+
+UPDATE `zof_mrp`.`productcutoptions`
+SET
+
+`OptionProductCutOptions` = 'Raglan'
+WHERE `Id` = 4;
