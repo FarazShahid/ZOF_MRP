6c0028e7697e6a4bcee967181b0813eaae7e40ad
arslanrasheedgenx <153821015+arslanrasheedgenx@users.noreply.github.com>
2025-07-18 15:51:17 +0500

    Size measurements new fields added

--- src/app/addsize/BottomUnit.tsx
+++ src/app/addsize/BottomUnit.tsx
@@ -6,13 +6,23 @@ const BottomUnit = () => {
   const BottomUnitInputs = [
     { id: 1, name: "Hip", label: "Hip" },
     { id: 2, name: "Waist", label: "Waist" },
-    { id: 3, name: "Outseam", label: "Outseam" },
-    { id: 4, name: "Inseam", label: "Inseam" },
-    { id: 5, name: "HemBottom", label: "Hem Bottom" },
-    {id: 6, name: "KneeWidth", label: "Knee Width"},
-     {id: 7, name: "bFrontRise", label: "Front Rise"},
-    {id: 8, name: "LegOpening", label: "Leg Opening"},
-    
+    { id: 3, name: "WasitStretch", label: "Wasit Stretch" }, // new field
+    { id: 4, name: "WasitRelax", label: "Wasit Relax" }, // new field
+    { id: 5, name: "Thigh", label: "Thigh" }, // new field
+    { id: 6, name: "KneeWidth", label: "Knee Width" },
+    { id: 7, name: "BackRise", label: "Back Rise" }, // new field
+    { id: 8, name: "bFrontRise", label: "Front Rise" },
+    { id: 9, name: "TotalLength", label: "Total Length" }, // new field
+    { id: 10, name: "WBHeight", label: "WB-Height" }, // new field
+    { id: 11, name: "bBottomWidth", label: "Bottom Width" }, // new field
+    { id: 12, name: "HemBottom", label: "Hem Bottom" },
+    { id: 13, name: "BottomOriginal", label: "Bottom Original" }, // new field
+    { id: 14, name: "BottomElastic", label: "Bottom Elastic" }, // new field
+    { id: 15, name: "Outseam", label: "Outseam" },
+    { id: 16, name: "Inseam", label: "Inseam" },
+    { id: 17, name: "LegOpening", label: "Leg Opening" },
+    { id: 18, name: "BottomCuffZipped", label: "Bottom Cuff Zipped" }, // new field
+    { id: 19, name: "BottomStraightZipped", label: "Bottom Straight Zipped" }, // new field
   ];
   return (
     <div className="grid grid-cols-4 gap-2 border-1 dark:border-gray-300 shadow-lg rounded-lg p-3">
--- src/app/addsize/TopUnit.tsx
+++ src/app/addsize/TopUnit.tsx
@@ -8,26 +8,38 @@ const TopUnit = () => {
     { id: 2, name: "FrontNeckDrop", label: "Front Neck Drop" },
     { id: 3, name: "ShoulderSeam", label: "Shoulder Seam" },
     { id: 4, name: "ShoulderSlope", label: "Shoulder Slope" },
-    { id: 5, name: "UpperChest", label: "Upper Chest" },
-    { id: 6, name: "LowerChest", label: "Lower Chest" },
-    { id: 7, name: "SleeveLength", label: "Sleeve Length" },
-    { id: 8, name: "SleeveOpening", label: "Sleeve Opening" },
-    { id: 9, name: "FrontLengthHPS", label: "Front Length HPS" },
-    { id: 10, name: "FrontRise", label: "Front Rise" },
-    { id: 11, name: "Hem", label: "Hem" },
-    { id: 12, name: "NeckSize", label: " NeckSize" },
-    { id: 13, name: "CollarHeight", label: " Collar Height" },
-    { id: 14, name: "CollarPointHeight", label: "Collar Point Height" },
-    { id: 15, name: "CollarStandLength", label: "Collar Stand Length" },
-    { id: 16, name: "AcrossShoulders", label: "Across Shoulders" },
-    { id: 17, name: "BackLengthHPS", label: "Back Length HPS" },
-    { id: 18, name: "BottomWidth", label: "Bottom Width" },
-    { id: 19, name: "StandHeightBack", label: "Stand Height Back" },
-    { id: 20, name: "SideVentFront", label: "Side Vent Front" },
-    { id: 21, name: "SideVentBack", label: "Side Vent Back" },
-    { id: 22, name: "PlacketLength", label: "Placket Length" },
-    { id: 23, name: "TwoButtonDistance", label: "Two Button Distance" },
-    { id: 24, name: "PlacketWidth", label: "Placket Width" },
+    { id: 5, name: "AcrossShoulders", label: "Across Shoulders" },
+    { id: 6, name: "NeckSize", label: " Neck Size" },
+    { id: 7, name: "Neckwidth", label: " Neck width" }, // new field
+    { id: 8, name: "CollarHeight", label: " Collar Height" },
+    { id: 9, name: "CollarPointHeight", label: "Collar Point Height" },
+    { id: 10, name: "CollarStandLength", label: "Collar Stand Length" },
+    {
+      id: 11,
+      name: "ColllarHeightCenterBack",
+      label: "Collar Height at Center Back",
+    }, // new field 
+    { id: 12, name: "CollarOpening", label: "Collar Opening" }, //new field
+    { id: 13, name: "UpperChest", label: "Upper Chest" },
+    { id: 14, name: "LowerChest", label: "Lower Chest" },
+    { id: 15, name: "SleeveLength", label: "Sleeve Length" },
+    { id: 16, name: "SleeveOpening", label: "Sleeve Opening" },
+    { id: 17, name: "ArmHole", label: "Arm Hole" },
+    { id: 18, name: "ArmHoleStraight", label: "Arm Hole Straight" }, // new field
+    { id: 19, name: "CuffHeight", label: "Cuff Height" }, // new field
+    { id: 20, name: "FrontLengthHPS", label: "Front Length HPS" },
+    { id: 21, name: "BackLengthHPS", label: "Back Length HPS" },
+    { id: 22, name: "FrontRise", label: "Front Rise" },
+    { id: 23, name: "Hem", label: "Hem" },
+    { id: 24, name: "BottomHem", label: "Bottom Hem" },
+    { id: 25, name: "BottomWidth", label: "Bottom Width" },
+    { id: 25, name: "BottomRib", label: "Bottom Rib" }, // new field
+    { id: 26, name: "StandHeightBack", label: "Stand Height Back" },
+    { id: 27, name: "SideVentFront", label: "Side Vent Front" },
+    { id: 28, name: "SideVentBack", label: "Side Vent Back" },
+    { id: 29, name: "PlacketLength", label: "Placket Length" },
+    { id: 30, name: "TwoButtonDistance", label: "Two Button Distance" },
+    { id: 31, name: "PlacketWidth", label: "Placket Width" },
   ];
   return (
     <div className="grid grid-cols-4 gap-2 border-1 dark:border-gray-300 shadow-lg rounded-lg p-3">
